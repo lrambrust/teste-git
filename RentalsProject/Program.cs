99e092776b4487e1899fdbe044394c2f6d901fdd
Lucas Ambrust <ambrust.lucas@gmail.com>
2019-03-02 15:37:32 -0300

    Projeto de estudo, controle aluguel de quartos

---
+++ RentalsProject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Student[] s = new Student[10];
+
+            //recebe os valores
+            for (int i = 1; i <= n; i++)
+            {
+                //variáveis para inserir no vetor
+                string name, email;
+                int room;
+
+                Console.WriteLine("Rent #" + i + ":");
+                //Nome
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+
+                //Email
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+
+                //Quarto
+                Console.Write("Room: ");
+                room = int.Parse(Console.ReadLine());
+
+                s[room] = new Student(name, email);
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Busy rooms:");
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (s[i] != null)
+                {
+                    Console.WriteLine(i + ": " + s[i]);
+                }
+
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ RentalsProject/Student.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace RentalsProject
+{
+    class Student
+    {
+
+        public int Room { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+
+            return Name + ", " + Email;
+        }
+    }
+}
